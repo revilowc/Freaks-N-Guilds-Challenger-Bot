28549c1f64b23978fdcf2f1a99d25a9a03093a8e
revilowc <82066214+revilowc@users.noreply.github.com>
2021-12-31 05:57:14 +0000

    Update main.py

--- main.py
+++ main.py
@@ -52,4 +52,4 @@ for filename in os.listdir('./cogs'):
 	if filename.endswith('.py'):
 		client.load_extension(f'cogs.{filename[:-3]}')
 
-client.run('ODUxOTg2NTAxMDM3MzI2Mzc3.YMAQQg.FO2_dg-zLR4o6NvPJsIHamKFVoc')
+client.run('INSERT BOT TOKEN HERE')
