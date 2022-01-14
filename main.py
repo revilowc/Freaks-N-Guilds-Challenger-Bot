c55ac815b9c699b218ce7883cb2204de9327fd77
revilowc <82066214+revilowc@users.noreply.github.com>
2022-01-14 04:30:50 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import os
 import time
 from discord.ext import commands
 
+#Add bot token here 
+TOKEN = ""
+
 intents = discord.Intents.all()
 client = commands.Bot(
     command_prefix=commands.when_mentioned_or('!'),
@@ -52,4 +55,4 @@ for filename in os.listdir('./cogs'):
 	if filename.endswith('.py'):
 		client.load_extension(f'cogs.{filename[:-3]}')
 
-client.run('INSERT BOT TOKEN HERE')
+client.run(TOKEN)
