9f2bc5a7935f1a84b07c6fb5463ee9d37936bbb1
revilowc <82066214+revilowc@users.noreply.github.com>
2022-01-20 04:18:17 +0000

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ async def on_message(message):
         await message.delete()
         await message.channel.send(f"Got it! {message.author.mention}'s wallet address has been stored as `'{message.content}'`!")
 
-    elif message.channel == enlistedchannel and message.content[:2].lower() != "0x" and message.author != client.user and godrole not in message.author.roles:
+    elif message.channel == enlistedchannel and message.content[:2].lower() != "0x" and godrole not in message.author.roles and not message.author.bot:
         await message.reply("Please refrain from talking in this channel. Only wallet addresses should be sent here!")
 
     await client.process_commands(message)
